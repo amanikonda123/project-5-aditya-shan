98b5b3b82d7f611999240259ce0652196daeea2d
Aditya Manikonda <adityamanikonda123@gmail.com>
2022-03-11 00:37:46 -0800

    3/11/22 - 12:36 AM - WIP cracked ground + fire - need to change cracked ground image - need to change where fire spawns

---
+++ src/Fire.java
@@ -0,0 +1,14 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Fire extends Obstacle {
+
+    private static final int FIRE_ANIMATION_PERIOD = 3;
+
+    public Fire(String id, Point position, List<PImage> images, int imageIndex) {
+        super(id, position, images, imageIndex, FIRE_ANIMATION_PERIOD);
+    }
+
+
+}
