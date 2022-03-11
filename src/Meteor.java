f357ffbf44e805aad0da3c70450d161bf52bf01f
shansingh14 <shansingh9045@gmail.com>
2022-03-11 00:29:58 -0800

    Added meteor class

---
+++ src/Meteor.java
@@ -0,0 +1,11 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Meteor extends Obstacle{
+    public Meteor(String id, Point position, List<PImage> images, int imageIndex, int animationPeriod) {
+        super(id, position, images, imageIndex, animationPeriod);
+    }
+
+
+}
